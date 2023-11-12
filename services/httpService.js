1189a3547176e55e12949eb3ba901cf1779a0af5
Narges <50104356+NargesKazemiMehr@users.noreply.github.com>
2023-11-12 17:06:30 -0500

    Update httpService.js

--- services/httpService.js
+++ services/httpService.js
@@ -1,25 +1,5 @@
 import axios from "axios";
 
-const urlEnv = document.URL.includes("localhost")
-  ? "development"
-  : document.URL.includes(".sec")
-  ? "Security"
-  : document.URL.includes("test")
-  ? "Staging"
-  : document.URL.includes("ava")
-  ? "Production"
-  : null;
-console.log(urlEnv);
-
-if (urlEnv === "Staging") {
-  axios.defaults.baseURL = process.env.REACT_APP_API_STAGING_URL;
-} else if (urlEnv === "Production") {
-  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-} else if (urlEnv === "Security") {
-  axios.defaults.baseURL = process.env.REACT_APP_API_SEC_URL;
-} else if (urlEnv === "development") {
-  axios.defaults.baseURL = process.env.REACT_APP_API_DEV_URL;
-}
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -29,7 +9,6 @@ axios.interceptors.response.use(null, (error) => {
 
   if (!expectedError) {
     console.log("Error", error);
-    //alert("یک خطای پیش بینی نشده رخ داده است");
   }
   return Promise.reject(error);
 });
