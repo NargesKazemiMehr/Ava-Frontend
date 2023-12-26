eae5fe69745c312b6c09146f78a6867b6fa092de
Narges <50104356+NargesKazemiMehr@users.noreply.github.com>
2023-12-26 17:42:21 -0500

    Update Step3.js

--- pages/Step3.js
+++ pages/Step3.js
@@ -1,78 +1,38 @@
-import React, { useState, useEffect } from "react";
-import http from "../services/httpService";
-import apiUrl from "../constants/api/APIUtils";
-import Styles from "../styles/Home.module.css";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import ReactTable from "../components/ReactTable";
-import { CRUDMode } from "../constants/CRUDMode";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-import * as types from "../state-management/actions/actionTypes";
-
+import http from '../services/httpService';
+import apiUrl from '../constants/api/APIUtils';
+import Styles from '../styles/Home.module.css';
+import ReactTable from '../components/ReactTable';
+import { CRUDMode } from '../constants/CRUDMode';
+import * as types from '../state-management/actions/actionTypes';
+import { EmailSection, SmsSection, MessageTable, useSendMessage } from '../components'; // Assuming these are exported from components
 
 export const Step3 = () => {
   const navigate = useNavigate();
-  const [showModal, setShowModal] = useState(false);
-  const [selected, setSelected] = useState();
-
-const EmailSection = ({ sendMessageSubject, sendMessageAttachment }) => {
-  // Email section JSX
-  // ...
-};
-
-const SmsSection = ({ sendMessageSubject }) => {
-  // SMS section JSX
-  // ...
-};
-
-const MessageTable = ({ sendMessageMode, sendMessageTo, sendMessageDomain, sendMessageTo_All, columns, selected }) => {
-  // Table JSX
-  // ...
-};
-
-const useSendMessage = (apiKey, dispatch) => {
-  const sendEmail = async (formData) => {
-    // Email sending logic
-  };
-
-  const sendSms = async (formData) => {
-    // SMS sending logic
-  };
-
-  return { sendEmail, sendSms };
-};
+  const dispatch = useDispatch();
+  const {
+    // ... all required states from Redux ...
+  } = useSelector((state) => state.SendMessageReducer, shallowEqual);
   
-const { sendEmail, sendSms } = useSendMessage(apiKey, dispatch);
-const createFormData = () => {
-  let formData = new FormData();
-  // Form data creation logic
-  return formData;
-};
-  const handleDialog = async () => {
-    setShowModal(false);
-    try {
-      const formData = createFormData(); // Create a separate function to handle form data creation
-      if (sendMessageType === "Email") {
-        await sendEmail(formData);
-      } else {
-        await sendSms(formData);
-      }
-      setAlert("success");
-      // Dispatch success action
-    } catch (error) {
-      setAlert("error");
-      // Dispatch error action
-    } finally {
-      setOpen(true);
-    }
-  };
+  const [showModal, setShowModal] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [alert, setAlert] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const { sendEmail, sendSms } = useSendMessage(apiKey, dispatch);
+
+  // ... rest of the component logic, including handleDialog and createFormData ...
 
   return (
     <>
       <div className={Styles.container}>
         {/* Modal and other components */}
-        {sendMessageType === "Email" ? (
+        {sendMessageType === 'Email' ? (
           <EmailSection sendMessageSubject={sendMessageSubject} sendMessageAttachment={sendMessageAttachment} />
         ) : (
           <SmsSection sendMessageSubject={sendMessageSubject} />
